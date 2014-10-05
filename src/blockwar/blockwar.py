1950e063de30c43b3636e9e7062e505c4c3a6d49
Stephen Stack <ss@stephenstack.com>
2014-10-05 16:46:41 -0500

    Unglobalizing all of the state.

--- src/blockwar/blockwar.py
+++ src/blockwar/blockwar.py
@@ -6,19 +6,8 @@ from pygame.locals import QUIT, KEYUP, MOUSEBUTTONUP, K_LEFT, K_ESCAPE, K_RIGHT,
     K_a, K_UP, K_d, K_DOWN, K_s
 from constants import *
 
+
 logger = logging.getLogger('blockwar')
-
-
-# Global state
-FPSCLOCK = None
-DISPLAYSURF = None
-BASICFONT = None
-RESET_SURF = None
-RESET_RECT = None
-NEW_SURF = None
-NEW_RECT = None
-SOLVE_SURF = None
-SOLVE_RECT = None
 
 
 class BlockWarException(Exception):
@@ -31,11 +20,23 @@ class QuitEvent(BlockWarException):
 
 class BlockWar(object):
     def __init__(self):
-        pass
+        self._fps_clock = None
+        self._display_surface = None
+        self._basic_font = None
+        self._reset_surface = None
+        self._reset_rect = None
+        self._new_surface = None
+        self._new_rect = None
+        self._solve_surface = None
+        self._solve_rect = None
+        self._solved_board = None
 
-    #------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------
     # Private
-    #------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------
+    def _update_display(self):
+        pygame.display.update()
+
     def _check_for_quit(self):
         if pygame.event.get(QUIT):
             raise QuitEvent()
@@ -130,25 +131,25 @@ class BlockWar(object):
         # draw a tile at board coordinates tilex and tiley, optionally a few
         # pixels over (determined by adjx and adjy)
         left, top = self._get_left_top_of_file(tilex, tiley)
-        pygame.draw.rect(DISPLAYSURF, TILECOLOR,
+        pygame.draw.rect(self._display_surface, TILECOLOR,
                          (left + adjx, top + adjy, TILESIZE, TILESIZE))
-        textSurf = BASICFONT.render(str(number), True, TEXTCOLOR)
+        textSurf = self._basic_font.render(str(number), True, TEXTCOLOR)
         textRect = textSurf.get_rect()
         textRect.center = left + int(TILESIZE / 2) + adjx, top + int(TILESIZE / 2) + adjy
-        DISPLAYSURF.blit(textSurf, textRect)
+        self._display_surface.blit(textSurf, textRect)
 
     def _make_text(self, text, color, bgcolor, top, left):
         # create the Surface and Rect objects for some text.
-        textSurf = BASICFONT.render(text, True, color, bgcolor)
-        textRect = textSurf.get_rect()
-        textRect.topleft = (top, left)
-        return textSurf, textRect
+        text_surface = self._basic_font.render(text, True, color, bgcolor)
+        text_rectangle = text_surface.get_rect()
+        text_rectangle.topleft = (top, left)
+        return text_surface, text_rectangle
 
     def _draw_board(self, board, message):
-        DISPLAYSURF.fill(BGCOLOR)
+        self._display_surface.fill(BGCOLOR)
         if message:
-            textSurf, textRect = self._make_text(message, MESSAGECOLOR, BGCOLOR, 5, 5)
-            DISPLAYSURF.blit(textSurf, textRect)
+            text_surface, text_rectangle = self._make_text(message, MESSAGECOLOR, BGCOLOR, 5, 5)
+            self._display_surface.blit(text_surface, text_rectangle)
 
         for tilex in range(len(board)):
             for tiley in range(len(board[0])):
@@ -158,12 +159,12 @@ class BlockWar(object):
         left, top = self._get_left_top_of_file(0, 0)
         width = BOARDWIDTH * TILESIZE
         height = BOARDHEIGHT * TILESIZE
-        pygame.draw.rect(DISPLAYSURF, BORDERCOLOR,
+        pygame.draw.rect(self._display_surface, BORDERCOLOR,
                          (left - 5, top - 5, width + 11, height + 11), 4)
 
-        DISPLAYSURF.blit(RESET_SURF, RESET_RECT)
-        DISPLAYSURF.blit(NEW_SURF, NEW_RECT)
-        DISPLAYSURF.blit(SOLVE_SURF, SOLVE_RECT)
+        self._display_surface.blit(self._reset_surface, self._reset_rect)
+        self._display_surface.blit(self._new_surface, self._new_rect)
+        self._display_surface.blit(self._solve_surface, self._solve_rect)
 
     def _slide_animation(self, board, direction, message, animation_speed):
         # Note: This function does not check if the move is valid.
@@ -184,7 +185,7 @@ class BlockWar(object):
 
         # prepare the base surface
         self._draw_board(board, message)
-        baseSurf = DISPLAYSURF.copy()
+        baseSurf = self._display_surface.copy()
         # draw a blank space over the moving tile on the baseSurf Surface.
         moveLeft, moveTop = self._get_left_top_of_file(movex, movey)
         pygame.draw.rect(baseSurf, BGCOLOR, (moveLeft, moveTop, TILESIZE, TILESIZE))
@@ -192,7 +193,7 @@ class BlockWar(object):
         for i in range(0, TILESIZE, animation_speed):
             # animate the tile sliding over
             self._check_for_quit()
-            DISPLAYSURF.blit(baseSurf, (0, 0))
+            self._display_surface.blit(baseSurf, (0, 0))
             if direction == UP:
                 self._draw_tile(movex, movey, board[movex][movey], 0, -i)
             if direction == DOWN:
@@ -203,7 +204,7 @@ class BlockWar(object):
                 self._draw_tile(movex, movey, board[movex][movey], i, 0)
 
             pygame.display.update()
-            FPSCLOCK.tick(FPS)
+            self._fps_clock.tick(FPS)
 
     def _reset_animation(self, board, all_moves):
         # make all of the moves in all_moves in reverse.
@@ -223,16 +224,16 @@ class BlockWar(object):
                                   animation_speed=int(TILESIZE / 2))
             self._make_move(board, oppositeMove)
 
-    def _generate_new_puzzle(self, numSlides):
+    def _generate_new_puzzle(self, num_slides):
         # From a starting configuration, make numSlides number of moves (and
         # animate these moves).
-        sequence = []
-        board = self._get_starting_board()
+        sequence = list()
+        board = self._get_starting_board()  # 2d array representing 4x4 tile values
         self._draw_board(board, '')
         pygame.display.update()
         pygame.time.wait(500)  # pause 500 milliseconds for effect
         lastMove = None
-        for i in range(numSlides):
+        for i in range(num_slides):
             move = self._get_random_move(board, lastMove)
             self._slide_animation(board, move, 'Generating new puzzle...',
                                   animation_speed=int(TILESIZE / 3))
@@ -241,10 +242,10 @@ class BlockWar(object):
             lastMove = move
         return board, sequence
 
-    def _tick(self, main_board, solution_seq, SOLVEDBOARD, all_moves):
+    def _tick(self, main_board, solution_seq, all_moves):
         slide_to = None  # the direction, if any, a tile should slide
         msg = 'Click tile or press arrow keys to slide.'  # contains the message to show in the upper left corner.
-        if main_board == SOLVEDBOARD:
+        if main_board == self._solved_board:
             msg = 'Solved!'
 
         self._draw_board(main_board, msg)
@@ -257,15 +258,15 @@ class BlockWar(object):
 
                 if (spotx, spoty) == (None, None):
                     # check if the user clicked on an option button
-                    if RESET_RECT.collidepoint(event.pos):
+                    if self._reset_rect.collidepoint(event.pos):
                         self._reset_animation(main_board,
                                               all_moves)  # clicked on Reset button
                         all_moves = []
-                    elif NEW_RECT.collidepoint(event.pos):
+                    elif self._new_rect.collidepoint(event.pos):
                         main_board, solution_seq = self._generate_new_puzzle(
                             80)  # clicked on New Game button
                         all_moves = []
-                    elif SOLVE_RECT.collidepoint(event.pos):
+                    elif self._solve_rect.collidepoint(event.pos):
                         self._reset_animation(main_board,
                                               solution_seq + all_moves)  # clicked on Solve button
                         all_moves = []
@@ -303,39 +304,41 @@ class BlockWar(object):
             self._make_move(main_board, slide_to)
             all_moves.append(slide_to)  # record the slide
         pygame.display.update()
-        FPSCLOCK.tick(FPS)
+        self._fps_clock.tick(FPS)
 
     #------------------------------------------------------------------------------------
     # Public
     #------------------------------------------------------------------------------------
     def setup(self):
-        global FPSCLOCK, DISPLAYSURF, BASICFONT, RESET_SURF, RESET_RECT, NEW_SURF, NEW_RECT, SOLVE_SURF, SOLVE_RECT
 
         pygame.init()
         pygame.display.set_caption('Block War')
 
-        FPSCLOCK = pygame.time.Clock()
-        DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-        BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
+        self._fps_clock = pygame.time.Clock()
+        self._display_surface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+        self._basic_font = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
 
         # Store the option buttons and their rectangles in OPTIONS.
-        RESET_SURF, RESET_RECT = self._make_text('Reset', TEXTCOLOR, TILECOLOR,
-                                                 WINDOWWIDTH - 120,
-                                                 WINDOWHEIGHT - 90)
-        NEW_SURF, NEW_RECT = self._make_text('New Game', TEXTCOLOR, TILECOLOR,
-                                             WINDOWWIDTH - 120,
-                                             WINDOWHEIGHT - 60)
-        SOLVE_SURF, SOLVE_RECT = self._make_text('Solve', TEXTCOLOR, TILECOLOR,
-                                                 WINDOWWIDTH - 120,
-                                                 WINDOWHEIGHT - 30)
+        self._reset_surface, self._reset_rect = self._make_text('Reset', TEXTCOLOR,
+                                                                TILECOLOR,
+                                                                WINDOWWIDTH - 120,
+                                                                WINDOWHEIGHT - 90)
+        self._new_surface, self._new_rect = self._make_text('New Game', TEXTCOLOR,
+                                                            TILECOLOR,
+                                                            WINDOWWIDTH - 120,
+                                                            WINDOWHEIGHT - 60)
+        self._solve_surface, self._solve_rect = self._make_text('Solve', TEXTCOLOR,
+                                                                TILECOLOR,
+                                                                WINDOWWIDTH - 120,
+                                                                WINDOWHEIGHT - 30)
 
     def run(self):
         logger.critical('Starting BlockWar...')
         main_board, solution_seq = self._generate_new_puzzle(80)
-        SOLVEDBOARD = self._get_starting_board()  # a solved board is the same as the board in a start state.
+        self._solved_board = self._get_starting_board()  # a solved board is the same as the board in a start state.
         all_moves = []  # list of moves made from the solved configuration
         while True:  # main game loop
-            self._tick(main_board, solution_seq, SOLVEDBOARD, all_moves)
+            self._tick(main_board, solution_seq, all_moves)
 
     def quit(self):
         """Quit Block War, must be called outside of the main execution loop"""
